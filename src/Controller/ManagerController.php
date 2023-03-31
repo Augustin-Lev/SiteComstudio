5a2fbe8a1b25426fc1d1cf401520ed2cb734fee9
Augustin <augustin.levasseur@etu.unilasalle.fr>
2023-03-31 20:28:52 +0200

    suppression de dossier

---
+++ src/Controller/ManagerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\LoginType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ManagerController extends AbstractController
+{
+
+   public function index(Request $request): Response
+   {
+    
+    require("C:\wamp64\www\studioSite\src\Entity\ManagerEntity.php");
+    return $this->render('blank.twig');
+   }
+
+}
---
+++ src/Entity/ManagerEntity.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+echo '
+    <script> 
+    alert("La suppression de cette catégorie entrainera la suppression de tous les dossiers et photos qu\'elle contient.
+    Si vous n\'êtes pas sûr de votre action, faite annuler.")
+    </script> ;';
+
+
+var_dump($_POST);
+$dossier = "C:\wamp64\www\studioSite\public\image/".$_POST["categorie"]."/".$_POST["dossier"];
+
+if (file_exists($dossier)){
+    $files = glob($dossier.'/*'); // get all file names
+    foreach($files as $file){ // iterate files
+    if(is_file($file))
+        unlink($file); // delete file
+    }
+
+    if (rmdir($dossier)){
+        echo "le dossier a bien été supprimé";
+    }else{
+        echo "une erreur est apparu";
+    }
+}else{
+    echo 'le dossier n\'existe déjà plus';
+}
